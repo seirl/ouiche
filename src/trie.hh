f1539d2ad3de609df8acb1f6ffc7456e8ccd1fd9
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2015-06-04 19:02:46 +0200

    wip: ptrie

--- src/trie.hh
+++ src/trie.hh
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <cstdio>
 #include <cassert>
 #include <iostream>
 #include <memory>
 #include <unordered_map>
+#include <vector>
 
 #ifndef NDEBUG
 # define DEBUG(fmt, args...) fprintf(stderr, "debug: " fmt "\n", ## args)
@@ -18,9 +20,9 @@ public:
     using matches_t =
         std::unordered_map<std::string, std::pair<unsigned, unsigned>>;
 
-    Trie()
+    Trie(unsigned freq = 0)
       : children_()
-      , freq_(0)
+      , freq_(freq)
     {
     }
 
@@ -45,15 +47,37 @@ public:
             freq_ = freq;
             return;
         }
-
-        auto it = children_.find(word[start]);
-        if (it == children_.end())
-            children_[word[start]] = std::make_unique<Trie>();
-        children_[word[start]]->add_word(freq, word, start + 1);
+        size_t node;
+        for (node = 0; node < children_.size(); node++)
+            if (children_[node].first[0] == word[start])
+                break;
+        if (node == children_.size()) // no edge with same starting char
+            children_.push_back({word.substr(start),
+                                 std::make_unique<Trie>(freq)});
+        else
+        {
+            auto& edge = children_[node];
+            size_t pos = *std::mismatch(word.begin() + start, word.end(),
+                                       edge.first.begin()).first;
+            if (pos == edge.first.size()) // edge is the prefix
+                edge.second->add_word(freq, word, start + edge.first.size());
+            else if (pos == word.size() - start)
+            {
+                // word is a prefix of the label, we need to put a node in the
+                // middle
+                //TODO(seirl)
+            }
+            else
+            {
+                // common prefix, we need to split and fork the edge
+                //TODO(seirl)
+            }
+        }
     }
 
     void serialize(std::ostream& out) const
     {
+        //TODO(seirl): adapt to ptrie
         size_t nb_children = children_.size();
         out.write(reinterpret_cast<const char*>(&freq_), sizeof (unsigned));
         out.write(reinterpret_cast<const char*>(&nb_children), sizeof (size_t));
@@ -66,6 +90,7 @@ public:
 
     static std::unique_ptr<Trie> deserialize(std::istream& in)
     {
+        //TODO(seirl): adapt to ptrie
         auto res = std::make_unique<Trie>();
 
         size_t nb_children;
@@ -82,6 +107,7 @@ public:
 
     unsigned lookup(const std::string& word, size_t start = 0) const
     {
+        //TODO(seirl): adapt to ptrie
         assert(start <= word.size());
         if (start == word.size())
             return freq_;
@@ -102,6 +128,7 @@ public:
 
     void format_dot(std::ostream& out) const
     {
+        //TODO(seirl): adapt to ptrie
         out << "digraph Trie {" << std::endl;
         format_dot_here_(out);
         out << "}" << std::endl;
@@ -110,6 +137,7 @@ public:
 private:
     void format_dot_here_(std::ostream& out) const
     {
+        //TODO(seirl): adapt to ptrie
         out << "    n" << this << " [label=\"";
         if (freq_)
             out << freq_;
@@ -132,6 +160,6 @@ private:
         //FIXME(seirl): complete the algorithm
     }
 
-    std::unordered_map<char, std::unique_ptr<Trie>> children_;
+    std::vector<std::pair<std::string, std::unique_ptr<Trie>>> children_;
     unsigned freq_; // 0 if not final
 };
